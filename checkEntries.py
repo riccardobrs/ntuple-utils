16915cf24a0b0eb1cfb8342010797fee74e2bddf
riccardobrs <riccardo158b@gmail.com>
2021-02-22 16:04:43 +0100

    Check number of entries

---
+++ checkEntries.py
@@ -0,0 +1,29 @@
+import ROOT
+from glob import glob
+import argparse
+
+if __name__ == '__main__':
+
+    print("""
+ _________________________ 
+  check number of entries 
+ ------------------------- """)
+
+    parser = argparse.ArgumentParser(description='Command line parser')
+    parser.add_argument('--baseFolder', '-b', dest='baseFolder', help='Folder containing ntuple', required=True)
+    parser.add_argument('--prefix', dest='prefix', help='.root files prefix', default='ntuple', required=False)
+    args = parser.parse_args()
+
+    baseFolder = os.path.abspath(args.baseFolder)
+    ntuple = glob (baseFolder + '/' + args.prefix + '*.root')
+
+    for n in ntuple:
+
+        f = ROOT.TFile (n)
+        objs = [f.Get(key.GetName()).IsA().GetName() for key in f.GetListOfKeys()]
+        names = [key.GetName() for key in f.GetListOfKeys()]
+        name = dict(zip(objs, names))['TNtuple']
+        t = f.Get(name)
+        entries = t.GetEntries()
+        print(name + ' ' + str(entries))
+        f.Close()
