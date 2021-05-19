21b7531bb020947006437891fceaa7d697fe6ff7
riccardobrs <riccardo158b@gmail.com>
2021-05-19 15:41:22 +0200

    manage root files

--- getXS.py
+++ getXS.py
@@ -3,6 +3,10 @@ from glob import glob
 import argparse
 import os
 import re
+import ROOT
+
+global sample_str
+sample_str = "samples['{0}'].extend( ['xsec={1}',      'kfact=1.00',           'ref=X'] )\n"
 
 def getRegexMatch(regex, str_analyzed):
 
@@ -21,7 +25,7 @@ def getSampleStr(cfgfile):
         sample_name = [str(c) for c in cfg.sections() if str(c).startswith('Z')][0]
         sample_XS = str(cfg.get(sample_name, 'XS')) # pb
 
-    return "samples['{0}'].extend( ['xsec={1}',      'kfact=1.00',           'ref=X'] )\n".format(sample_name,sample_XS)
+    return sample_str.format(sample_name,sample_XS)
 
 
 if __name__ == '__main__':
@@ -30,6 +34,7 @@ if __name__ == '__main__':
     parser.add_argument('-b', dest='base', help='Results archives base folder', required=True)
     parser.add_argument('-o', dest='outfile', help='Output path', required=True)
     parser.add_argument('--ext', dest='ext', help='Archives extension', required=False, default='tar.gz')
+    parser.add_argument('--root', dest='root', help='Get XS from .root file', required=False, default=False, action='store_true')
     args = parser.parse_args()
 
     xs_list = list()
@@ -94,5 +99,23 @@ if __name__ == '__main__':
 
         with open(args.outfile, 'a') as o:
             o.write(getSampleStr(cfgpath))
+    
+    if args.root:
+        roots = glob(os.path.abspath(args.base) + '/*.root')
+        for root in roots:
+            sample = getRegexMatch("(?<=ntuple_).*?(?=.root)", os.path.basename(root))
+            if sample in xs_list:
+                print('[INFO] skipping ' + root)
+                continue
+            print('[INFO] working on ' + root)
+            f = ROOT.TFile(root, 'READ')
+            h = f.Get(sample + '_nums')
+            xs = h.GetBinContent(1)
+            to_write = sample_str.format(sample,xs)
+            f.Close()
+
+            with open(args.outfile, 'a') as o:
+                o.write(to_write)
+
 
     
